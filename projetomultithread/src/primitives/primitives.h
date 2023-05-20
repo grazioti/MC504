4ec6a7ff4e1300d6c13e67b3a49506aa3a2d4f43
Joao Miguel <jm.og@hotmail.com>
2023-05-20 14:56:06 -0300

    refactor: refactor Queue, Person and Cart primitives

---
+++ projetomultithread/src/primitives/primitives.h
@@ -0,0 +1,36 @@
+#include "../buffer.h"
+
+typedef struct Person {
+    char *body;
+    char *mask;
+    char value;
+    int x, y;
+} Person;
+
+typedef struct Queue {
+    int x, y;
+    int max_size, n;
+    Person *people;
+} Queue;
+
+typedef struct Cart {
+    char *body;
+    char *mask;
+    char *empty_mask;
+    char value;
+    int x, y;
+} Cart;
+
+// Person Methods
+void draw_person(Person p, PrintBuffer *buffer);
+
+// Queue Methods
+void init_queue(Queue *l, int x, int y, int max_size);
+void add_person(Queue *l, int value, , char *person_mask);
+void remove_person(Queue *l, int value);
+void draw_line(Queue l, PrintBuffer *buffer);
+
+// Cart Methods
+void init_cart(Cart *c, char *mask);
+void set_value(Cart *c, int value);
+void draw_cart(Cart *c, PrintBuffer *buffer);
